5468e01708272f121245a748238732cb2572905a
Ykanber <43858267+Ykanber@users.noreply.github.com>
2021-05-28 13:04:33 +0300

    Delete tstats.py

--- tstats.py
+++
@@ -1,6 +0,0 @@
-class Tstats:
-    def __init__(self, count,average,name,surname):
-        self.count = count
-        self.average = average
-        self.name = name
-        self.surname = surname
