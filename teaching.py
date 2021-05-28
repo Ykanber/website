4a591ad6a76a8dac9d4faed6410f72aac7c4eaec
Ykanber <43858267+Ykanber@users.noreply.github.com>
2021-05-28 13:04:28 +0300

    Delete teaching.py

--- teaching.py
+++
@@ -1,8 +0,0 @@
-class Teaching:
-    def __init__(self,T_username,T_Name,T_Surname,S_username,S_Name,S_Surname):
-        self.T_username = T_username
-        self.T_Name = T_Name
-        self.T_Surname = T_Surname
-        self.S_username = S_username
-        self.S_Name = S_Name
-        self.S_Surname = S_Surname
